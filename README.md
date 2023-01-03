3c8c2b89e546c391a5a31c06633560c2ea238e02
Murillo Do Carmo <murillodocarmo@gmail.com>
2023-01-03 05:08:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# valorcotacaocacau
+ Simples código em node.js para recuperar o valor da cotação do cacau em um site específico.
---
+++ bin/index.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  // require browser
+  const browser = await puppeteer.launch();
+  // new page
+  const page = await browser.newPage();
+  // goto webadress
+  await page.goto('https://www.mercadodocacau.com.br/cotacoes');
+  // read page
+  const data = await page.evaluate(() => {
+    // find all html elements
+    return document.querySelector('*').outerHTML;
+  });
+  // log entire html
+  console.log(data);
+
+  // replace all enter keywords
+  var simpleData = data.replace(/\n/g, "");
+  // create a regexp find data
+  var myRegexp = new RegExp(`<span>BA</span></li>(.*?)<li class="box box-4 left b-right-1-dashed">(.*?)<span>(.*?)</span></li>`, 'gm');
+  // execute regexp
+  var match = myRegexp.exec(simpleData);
+  // log final data
+  console.log(`Cacau Bahia/@: R$${match[3]}`);
+
+  // print a screenshot
+  await page.screenshot({ path: `cotacao.png`, fullPage: true });
+  // close browser
+  await browser.close();
+})();
